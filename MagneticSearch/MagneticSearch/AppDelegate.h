9aa3ab92ed92d37a9c05396355d07e0ca8677480
xiaxia <728599123@qq.com>
2017-02-21 17:50:45 +0800

    初始化项目

---
+++ MagneticSearch/MagneticSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagneticSearch
+//
+//  Created by xiaxia on 2017/2/21.
+//  Copyright © 2017年 xiaxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagneticSearch/MagneticSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagneticSearch
+//
+//  Created by xiaxia on 2017/2/21.
+//  Copyright © 2017年 xiaxia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
